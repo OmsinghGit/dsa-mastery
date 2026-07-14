985edf468159152ccba645037c029440285265a7
Om Singh <omsinghleet@gmail.com>
2026-07-14 22:30:40 +0530

    Adding resolved questions

--- 01-Arrays/easy/0002_Sqrt.cpp
+++ 01-Arrays/easy/0002_Sqrt.cpp
@@ -14,39 +14,37 @@ Date         : 14-07-2026
 Author       : Om Singh
 ===============================================================================
 */
+#include <iostream>
+using namespace std;
+
 class Solution {
 public:
     int mySqrt(int x) {
-
         int s = 0;
         int e = x;
-
         int ans = -1;
 
-        while(s <= e)
-        {
-            int mid = s + (e-s)/2;
-
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
             long long square = 1LL * mid * mid;
 
-            // Ab yahan 3 conditions tum likhoge
-
-           if(square == x)
-{
-    return mid;
-}
-else if(square < x)
-{
-    ans = mid;
-    s = mid + 1;
-}
-else
-{
-    e = mid - 1;
-}
-
+            if (square == x)
+                return mid;
+            else if (square < x) {
+                ans = mid;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
         }
 
         return ans;
     }
 };
+
+int main() {
+    Solution obj;
+    cout << obj.mySqrt(8) << endl;
+
+    return 0;
+}

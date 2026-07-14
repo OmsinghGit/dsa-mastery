ec5af4b0eebda9dc58d4e08a21d9d2d83b8b5f2b
Om Singh <omsinghleet@gmail.com>
2026-07-14 12:07:01 +0530

    Adding Pivot element questions

--- 01-Arrays/medium/0001_Find_Pivot_Element.cpp
+++ 01-Arrays/medium/0001_Find_Pivot_Element.cpp
@@ -15,19 +15,24 @@ Author       : Om Singh
 ===============================================================================
 */
 
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-void solve()
+int getPivot(int arr[], int n)
 {
+    int s=0, e=n-1, mid = s+(e-s)/2;
+    while(s<e)
+    {
+        if(arr[mid] >= arr[0])
+        s=mid+1;
+        else e=mid;
 
+         mid = s+(e-s)/2;
+    }
+    return s;
 }
 
-int main()
+int main() 
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
+    int arr[5] = {3,8,10,17,1};
+    cout << "Pivot is: " << getPivot(arr, 5) << endl;
 }

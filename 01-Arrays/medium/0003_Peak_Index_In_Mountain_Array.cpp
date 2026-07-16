fcc3e33d3a410ccc26636afa833b46ee534c8314
Om Singh <omsinghleet@gmail.com>
2026-07-16 13:17:08 +0530

    Adding

--- 01-Arrays/medium/0003_Peak_Index_In_Mountain_Array.cpp
+++ 01-Arrays/medium/0003_Peak_Index_In_Mountain_Array.cpp
@@ -14,21 +14,16 @@ Date         : 16-07-2026
 Author       : Om Singh
 ===============================================================================
 */
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve()
-{
-
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+       int s=0, e= arr.size()-1;
+       while (s<e)
+       {
+        int mid=s+(e-s)/2;
+        if (arr[mid] < arr[mid+1]) s=mid+1;
+        else e=mid;
+       }
+       return s;
+    }
+};

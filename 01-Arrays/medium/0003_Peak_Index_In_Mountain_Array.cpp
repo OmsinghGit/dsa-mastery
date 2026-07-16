8fa09e718770ed8fd95f56ccde66f916720be284
Om Singh <omsinghleet@gmail.com>
2026-07-16 13:14:53 +0530

    Adding questions

---
+++ 01-Arrays/medium/0003_Peak_Index_In_Mountain_Array.cpp
@@ -0,0 +1,34 @@
+/*
+===============================================================================
+Problem      : Peak Index In Mountain Array
+Platform     : LeetCode
+Pattern      : Arrays
+Difficulty   : Medium
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Date         : 16-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

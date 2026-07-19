44e78c2db0ac8320e87f614a5d84b5ee56b26b8d
Om Singh <omsinghleet@gmail.com>
2026-07-19 10:11:36 +0530

    Adding new codes

---
+++ 10-Stack/Medium/0001_Smallest_Susequences_of_Distict_Characters.cpp
@@ -0,0 +1,34 @@
+/*
+===============================================================================
+Problem      : Smallest Susequences of Distict Characters
+Platform     : LeetCode
+Pattern      : Stack
+Difficulty   : Medium
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Date         : 19-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

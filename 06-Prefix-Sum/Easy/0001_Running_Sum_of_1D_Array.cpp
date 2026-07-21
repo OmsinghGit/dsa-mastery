249951cd5e425a0d9a47f479aa1281fabbc72748
Om Singh <omsinghleet@gmail.com>
2026-07-21 20:38:00 +0530

    Adding new questions

---
+++ 06-Prefix-Sum/Easy/0001_Running_Sum_of_1D_Array.cpp
@@ -0,0 +1,24 @@
+/*
+===============================================================================
+Problem      : Running Sum of 1D Array
+Platform     : LeetCode
+Pattern      : Prefix Sum
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 21-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        
+    }
+};

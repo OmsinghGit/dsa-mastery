365e112498e29d4bb5aa6ca8cceb0f453109c659
Om Singh <omsinghleet@gmail.com>
2026-07-21 20:57:51 +0530

    Adding files

--- 06-Prefix-Sum/Easy/0001_Running_Sum_of_1D_Array.cpp
+++ 06-Prefix-Sum/Easy/0001_Running_Sum_of_1D_Array.cpp
@@ -19,6 +19,12 @@ Author       : Om Singh
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        
+
+        for(int i = 1; i < nums.size(); i++)
+        {
+            nums[i] += nums[i-1];
+        }
+
+        return nums;
     }
 };

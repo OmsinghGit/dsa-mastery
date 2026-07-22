4f41aac5f3dbfb6e110d09b9ce0144da568ff67e
Om Singh <omsinghleet@gmail.com>
2026-07-22 22:43:36 +0530

    Adding string question

---
+++ 02-Strings/Easy/0001_Maximum_number_of_words_found_in_Sentences.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Maximum number of words found in Sentences
+Platform     : LeetCode
+Pattern      : Strings
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 22-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

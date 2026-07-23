7ed77919f7b134454d6f8f82a71f79473ad68456
Om Singh <omsinghleet@gmail.com>
2026-07-23 20:49:09 +0530

    Adding solutions: Approaches

--- 24-Matrix/Easy/0001_Richest_Health_Count.cpp
+++ 24-Matrix/Easy/0001_Richest_Health_Count.cpp
@@ -5,12 +5,17 @@ Platform     : LeetCode
 Pattern      : Matrix
 Difficulty   : Easy
 
-Approach     :
+Approach     :  Traverse each row of the matrix and calculate the sum of all elements in that row. Compare the row
+                sum with the current maximum wealth and update it if the current sum
+                is greater. After traversing all rows, return the maximum wealth.
 
-Time Complexity  :
-Space Complexity :
+Time Complexity  : O(mn)
+Space Complexity : O(1)
 
-Interview Explanation : 
+Interview Explanation : Each row represents one customer, and each column represents the money in one bank
+                        account. I calculate the total wealth of every customer by adding all the values in that row. 
+                        Then I compare it with the maximum wealth found so far. After checking all customers, I return 
+                        the maximum wealth.
 
 Date         : 23-07-2026
 Author       : Om Singh

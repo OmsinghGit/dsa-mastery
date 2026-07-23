8af95ef291150b4398efeaf0a6e605c247067bce
Om Singh <omsinghleet@gmail.com>
2026-07-23 20:56:30 +0530

    Adding medium binary search questions

---
+++ 07-Binary-Search/Medium/0002_Koko_Eating_Bananas.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Koko Eating Bananas
+Platform     : LeetCode
+Pattern      : Binary Search
+Difficulty   : Medium
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 23-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

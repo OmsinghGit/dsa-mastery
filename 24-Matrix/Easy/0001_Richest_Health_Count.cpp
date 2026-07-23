53f5e5c14a85e19a0b0b45ff746f66b7d4c73c31
Om Singh <omsinghleet@gmail.com>
2026-07-23 20:19:16 +0530

    Adding matrix easy question

---
+++ 24-Matrix/Easy/0001_Richest_Health_Count.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Richest Health Count
+Platform     : LeetCode
+Pattern      : Matrix
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 23-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

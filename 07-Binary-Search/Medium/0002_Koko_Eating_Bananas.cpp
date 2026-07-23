d2174be7dab6d1a91f3404937f8b10f1233250b1
Om Singh <omsinghleet@gmail.com>
2026-07-23 20:57:15 +0530

    Adding files

--- 07-Binary-Search/Medium/0002_Koko_Eating_Bananas.cpp
+++ 07-Binary-Search/Medium/0002_Koko_Eating_Bananas.cpp
@@ -17,20 +17,3 @@ Author       : Om Singh
 ===============================================================================
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve()
-{
-
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}

014157f855b9b448b7bf04647e6c45c53f4953de
Om Singh <omsinghleet@gmail.com>
2026-07-24 07:38:01 +0530

    Solution and codes: Koko Banana

--- 07-Binary-Search/Medium/0002_Koko_Eating_Bananas.cpp
+++ 07-Binary-Search/Medium/0002_Koko_Eating_Bananas.cpp
@@ -43,3 +43,63 @@ Author       : Om Singh
 //     }
 //     return ans;
 // }
+
+
+
+
+class Solution 
+{
+    public:
+    // Helper function to calculate total hours
+    // required to eat all banana piles at a given speed.
+    long long int calculateHours(vector<int>& piles, int speed)
+    {
+        long long totalHours=0;
+        for (int i=0;i<piles.size(); i++)
+        {
+            //Ceiling Division
+            totalHours += ( 1LL * piles[i] + speed - 1) / speed;
+        }
+        return totalHours;
+    }
+    int minEatingSpeed(vector<int>& piles, int h)
+    {
+        int low=1, high=*max_element(piles.begin(),piles.end()), ans=high;
+        while (low<=high)
+        {
+            int mid=low+(high-low)/2;
+            long long totalHours = calculateHours(piles,mid);
+            if (totalHours<=h)
+            {
+                ans=mid,high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

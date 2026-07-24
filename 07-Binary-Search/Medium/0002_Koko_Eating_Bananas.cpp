abef3df68630179e038a07f6676314a3245f2c95
Om Singh <omsinghleet@gmail.com>
2026-07-24 07:20:02 +0530

    Adding: Brute force code Approach

--- 07-Binary-Search/Medium/0002_Koko_Eating_Bananas.cpp
+++ 07-Binary-Search/Medium/0002_Koko_Eating_Bananas.cpp
@@ -5,7 +5,7 @@ Platform     : LeetCode
 Pattern      : Binary Search
 Difficulty   : Medium
 
-Approach     :
+Approach     : Through binary search, 
 
 Time Complexity  :
 Space Complexity :
@@ -17,3 +17,29 @@ Author       : Om Singh
 ===============================================================================
 */
 
+// | - > basic approach code using helper and Binary Search
+
+// func(arr,speed)
+// {
+//     totalHours=0;
+//     for (int i=0; i<arr.size(); i++)
+//     {
+//         totalHours+=ciel((double)arr[i]/hours);
+//     }
+//     return totalHours;
+// }
+// BinarySearch (arr,h)
+// {
+//     low=1,high=maxElem(arr), ans;
+//     while(low<=high)
+//     {
+//         mid=low+(high-low)/2;
+//         totalHours=func(arr,mid);
+//         if (totalHours<=targetHours){
+//             ans=mid;
+//             high=mid-1;
+//         }
+//         else low=mid+1;
+//     }
+//     return ans;
+// }

592c58a4918e78067ac59a25d558cb569dbf8c04
Om Singh <omsinghleet@gmail.com>
2026-07-25 09:49:43 +0530

    Binary Search medium questions before Book Allocation Problem

---
+++ 07-Binary-Search/Medium/0003_Finding_the_Smallest_Divisor_given_a_Threshold.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Finding the Smallest Divisor given a Threshold
+Platform     : LeetCode
+Pattern      : Binary Search
+Difficulty   : Medium
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 25-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

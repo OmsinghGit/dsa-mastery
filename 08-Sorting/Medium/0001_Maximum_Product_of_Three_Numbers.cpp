904183f8974dc6b86df3d9ac3d4baddac1e33d73
Om Singh <omsinghleet@gmail.com>
2026-07-26 17:52:55 +0530

    Changes

--- 08-Sorting/Medium/0001_Maximum_Product_of_Three_Numbers.cpp
+++ 08-Sorting/Medium/0001_Maximum_Product_of_Three_Numbers.cpp
@@ -16,21 +16,3 @@ Date         : 26-07-2026
 Author       : Om Singh
 ===============================================================================
 */
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve()
-{
-
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}

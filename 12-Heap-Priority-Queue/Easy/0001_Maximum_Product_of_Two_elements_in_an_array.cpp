7f020863b66f194a4f406d9343564c3fdc00b7e8
Om Singh <omsinghleet@gmail.com>
2026-07-27 09:29:27 +0530

    Heap / Prority Queue

---
+++ 12-Heap-Priority-Queue/Easy/0001_Maximum_Product_of_Two_elements_in_an_array.cpp
@@ -0,0 +1,19 @@
+/*
+===============================================================================
+Problem      : Maximum Product of Two elements in an array
+Platform     : LeetCode
+Pattern      : Heap / Priority Queue
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 27-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+

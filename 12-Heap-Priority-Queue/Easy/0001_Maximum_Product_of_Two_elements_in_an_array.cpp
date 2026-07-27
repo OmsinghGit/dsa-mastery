66ea99a08401de7a8485c2ac5e0a88acb5d8cad0
Om Singh <omsinghleet@gmail.com>
2026-07-27 10:09:59 +0530

    files for practuce

--- 12-Heap-Priority-Queue/Easy/0001_Maximum_Product_of_Two_elements_in_an_array.cpp
+++ 12-Heap-Priority-Queue/Easy/0001_Maximum_Product_of_Two_elements_in_an_array.cpp
@@ -17,3 +17,22 @@ Author       : Om Singh
 ===============================================================================
 */
 
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+
+        priority_queue<int> pq;
+
+        for(int num : nums)
+        {
+            pq.push(num);
+        }
+
+        int first = pq.top();
+        pq.pop();
+
+        int second = pq.top();
+
+        return (first - 1) * (second - 1);
+    }
+};

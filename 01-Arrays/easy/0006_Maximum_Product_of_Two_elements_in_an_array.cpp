6cb386920d8ea25210ec282d947c190517d6c1d6
Om Singh <omsinghleet@gmail.com>
2026-07-27 10:15:10 +0530

    Switching questions to array codes

---
+++ 01-Arrays/easy/0006_Maximum_Product_of_Two_elements_in_an_array.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Maximum Product of Two elements in an array
+Platform     : LeetCode
+Pattern      : Arrays
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 27-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

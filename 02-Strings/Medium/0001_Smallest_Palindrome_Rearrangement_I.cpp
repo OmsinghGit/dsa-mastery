6e3ac366ce3f5ab8baca4de18e44ca6804b3aa24
Om Singh <omsinghleet@gmail.com>
2026-07-28 09:52:09 +0530

    Maintaining questions

---
+++ 02-Strings/Medium/0001_Smallest_Palindrome_Rearrangement_I.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Smallest Palindrome Rearrangement I
+Platform     : LeetCode
+Pattern      : Strings
+Difficulty   : Medium
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 28-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

4bd5efa6c0347e4c6226ac04c73833e65dbb5141
Om Singh <omsinghleet@gmail.com>
2026-07-28 09:53:00 +0530

    Adding questions

--- 02-Strings/Medium/0001_Smallest_Palindrome_Rearrangement_I.cpp
+++ 02-Strings/Medium/0001_Smallest_Palindrome_Rearrangement_I.cpp
@@ -16,21 +16,3 @@ Date         : 28-07-2026
 Author       : Om Singh
 ===============================================================================
 */
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve()
-{
-
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}

ac8c34fde1a522a561d75f41271d5c014ad6ff30
Om Singh <omsinghleet@gmail.com>
2026-07-28 13:13:30 +0530

    My solution: string palindrome

--- 02-Strings/Medium/0001_Smallest_Palindrome_Rearrangement_I.cpp
+++ 02-Strings/Medium/0001_Smallest_Palindrome_Rearrangement_I.cpp
@@ -16,3 +16,29 @@ Date         : 28-07-2026
 Author       : Om Singh
 ===============================================================================
 */
+
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        vector<int> freq(26,0);
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+        string firstHalf =  "";
+        string middle = "";
+        for (int i=0; i<26; i++)
+        {
+            for (int j=0; j<freq[i]/2; j++)
+            {
+                firstHalf += ('a'+i);
+            }
+            if (freq[i]%2==1)
+                {
+                    middle += ('a' + i);
+                }
+        }
+        string lastHalf = firstHalf;
+        reverse(lastHalf.begin(), lastHalf.end()); 
+        return firstHalf + middle + lastHalf;
+    }
+};

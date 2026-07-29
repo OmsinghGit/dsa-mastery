c6673badd2cfbca56a4491402160927d8d7d545c
Om Singh <omsinghleet@gmail.com>
2026-07-29 10:24:47 +0530

    Palindrome Rearrangement

---
+++ 02-Strings/Hard/0001_Smallest_Palindrome_Rearrangement_II.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Smallest Palindrome Rearrangement II
+Platform     : LeetCode
+Pattern      : Strings
+Difficulty   : Hard
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 29-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

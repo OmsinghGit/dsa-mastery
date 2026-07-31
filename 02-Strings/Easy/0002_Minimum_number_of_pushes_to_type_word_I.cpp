881cc57ce2b82140f646fc64009c0698edf9cb75
Om Singh <omsinghleet@gmail.com>
2026-07-31 05:46:28 +0530

    Finalizing string

--- 02-Strings/Easy/0002_Minimum_number_of_pushes_to_type_word_I.cpp
+++ 02-Strings/Easy/0002_Minimum_number_of_pushes_to_type_word_I.cpp
@@ -5,12 +5,19 @@ Platform     : LeetCode
 Pattern      : Strings
 Difficulty   : Easy
 
-Approach     :
+Approach     :  Since all characters are distinct, assign each new character to the position requiring the minimum number of pushes.
+                The first 8 characters take 1 push each, the next 8 take 2 pushes each, and so on.
+                Traverse the string and add the cost of each character using:
+                    > (i/8)+1;
+                Return the total minimum pushes.
 
-Time Complexity  :
-Space Complexity :
+Time Complexity  :  O(n) > Traverse the string once and calculate the push count for each character.
 
-Interview Explanation : 
+Space Complexity :  O(1) > Only a few variables are used. No extra data structure is required.
+
+Interview Explanation :     Since all characters are distinct, I greedily assign them to the positions requiring the fewest pushes.
+                            The first 8 characters need 1 push, the next 8 need 2 pushes, and so on. For each character at index i,
+                            the required pushes are (i / 8) + 1. Summing these values gives the minimum total pushes.
 
 Date         : 30-07-2026
 Author       : Om Singh

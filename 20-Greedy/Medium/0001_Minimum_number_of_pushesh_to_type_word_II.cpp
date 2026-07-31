b58a5e9ca037451c46c6196a275efe23fcb1022d
Om Singh <omsinghleet@gmail.com>
2026-07-31 05:50:07 +0530

    Greedy medium string question

---
+++ 20-Greedy/Medium/0001_Minimum_number_of_pushesh_to_type_word_II.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Minimum number of pushesh to type word II
+Platform     : LeetCode
+Pattern      : Greedy
+Difficulty   : Medium
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 31-07-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

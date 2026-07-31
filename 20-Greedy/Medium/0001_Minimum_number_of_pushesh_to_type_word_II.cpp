99fa1f38f65fea2bdc83e010c4779e9ecaa4c14a
Om Singh <omsinghleet@gmail.com>
2026-07-31 06:33:37 +0530

    Solution

--- 20-Greedy/Medium/0001_Minimum_number_of_pushesh_to_type_word_II.cpp
+++ 20-Greedy/Medium/0001_Minimum_number_of_pushesh_to_type_word_II.cpp
@@ -18,3 +18,20 @@ Author       : Om Singh
 */
 
 
+class Solution {
+public:
+    int minimumPushes(string word) {
+        long long ans = 0;
+        vector<int> freq(26,0);
+        for (char ch: word)
+        {
+            freq[ch-'a']++;
+        }
+        sort(freq.begin(), freq.end(), greater<int>());
+        for (int i=0; i<freq.size(); i++)
+        {
+            ans += freq[i] * ((i / 8) + 1);
+        }
+        return ans;
+    }
+};

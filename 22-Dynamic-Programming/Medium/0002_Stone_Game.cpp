f2e03c7c8cfc6a1e75a0682858884dbc25e5c5ae
Om Singh <omsinghleet@gmail.com>
2026-08-02 18:59:02 +0530

    Solving questions

--- 22-Dynamic-Programming/Medium/0002_Stone_Game.cpp
+++ 22-Dynamic-Programming/Medium/0002_Stone_Game.cpp
@@ -18,9 +18,25 @@ Author       : Om Singh
 */
 
 /*  I solved this question for maintaing leetcode streak, Later I will solve this problem again.
-
     Note - This is copied solved for maintaing streak*/
 
-    
 
 
+class Solution {
+public:
+
+    int dp[501][501];
+    int solve (vector<int>& piles, int i, int j)
+    {
+        if (i>j) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int takeLeft = piles[i] - solve(piles, i+1, j);
+        int takeRight = piles[j] - solve(piles, i, j-1);
+        return dp[i][j] = max(takeLeft, takeRight);
+    }
+    bool stoneGame(vector<int>& piles) {
+        memset(dp, -1, sizeof(dp));
+        return solve(piles, 0, piles.size()-1) > 0;
+    }
+};

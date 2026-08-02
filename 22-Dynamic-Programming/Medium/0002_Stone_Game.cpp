831597658b265fca2b4a9baffd8b8ccd78f95f92
Om Singh <omsinghleet@gmail.com>
2026-08-02 15:01:27 +0530

    Adding second dp problem

---
+++ 22-Dynamic-Programming/Medium/0002_Stone_Game.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Stone Game
+Platform     : LeetCode
+Pattern      : Dynamic Programming
+Difficulty   : Medium
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 02-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

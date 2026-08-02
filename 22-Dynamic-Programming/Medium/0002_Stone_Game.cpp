18397959c6a268df94d5ab3f28049d0410d3ede3
Om Singh <omsinghleet@gmail.com>
2026-08-02 15:02:04 +0530

    Comitting modification

--- 22-Dynamic-Programming/Medium/0002_Stone_Game.cpp
+++ 22-Dynamic-Programming/Medium/0002_Stone_Game.cpp
@@ -17,20 +17,4 @@ Author       : Om Singh
 ===============================================================================
 */
 
-#include <bits/stdc++.h>
-using namespace std;
 
-void solve()
-{
-
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}

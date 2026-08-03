7bda31cf25e5a9709ec3862b2f48e0f313e1dbd1
Om Singh <omsinghleet@gmail.com>
2026-08-03 06:06:33 +0530

    Adding Solution of Stone Game

--- 22-Dynamic-Programming/Hard/0001_Stone_Game_III.cpp
+++ 22-Dynamic-Programming/Hard/0001_Stone_Game_III.cpp
@@ -22,4 +22,36 @@ Author       : Om Singh
 /*  I solved this question for maintaing leetcode streak, Later I will solve this problem again.
     Note - This is copied solved for maintaing streak*/
 
-    
+
+class Solution 
+{
+    public:
+    int dp[50001];
+    int solve (vector<int>& stoneValue, int i)
+    {
+        if (i >= stoneValue.size()) return 0;
+        if (dp[i] != INT_MIN)
+        return dp[i];
+        int take = 0;
+        int best = INT_MIN;
+        for (int k = 0; k<3 && i+k < stoneValue.size(); k++)
+        {
+            take += stoneValue[i+k];
+            best = max(best, take - solve(stoneValue, i+k+1));
+        }
+        return dp[i] = best;
+    }
+
+    string stoneGameIII(vector<int>& stoneValue)
+    {
+        for (int i=0; i<50001; i++)
+            dp[i] = INT_MIN;
+
+            int diff  = solve(stoneValue, 0);
+
+            if(diff > 0) return "Alice";
+            else if(diff < 0) return "Bob";
+            else return "Tie";
+    }
+
+};

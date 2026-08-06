67c44d57063acefc4c4dffc140e5a5a8f22503d5
Om Singh <omsinghleet@gmail.com>
2026-08-06 12:26:17 +0530

    Adding solution with complexities

--- 23-Math/Easy/0007_Smallest_Divisible_Digit_Product_I.cpp
+++ 23-Math/Easy/0007_Smallest_Divisible_Digit_Product_I.cpp
@@ -5,12 +5,20 @@ Platform     : LeetCode
 Pattern      : Math
 Difficulty   : Easy
 
-Approach     :
+Approach     :  > Start checking from n.
+                > Find the product of all digits of the current number.
+                > If the product is divisible by t, return the current number.
+                > Otherwise, increment the current number and repeat the process until a valid number is found.
 
-Time Complexity  :
-Space Complexity :
 
-Interview Explanation : 
+Time Complexity  :  O(k × d) > k = number of integers checked until the answer is found.
+                             > d = number of digits in each number.
+                            For every number, we traverse all its digits once.
+
+Space Complexity :  O(1) > Only a few variables (current, temp, product, digit) are used.
+
+Interview Explanation : use a brute-force approach. Starting from n, I calculate the product of its digits. If the product is divisible by t, I return the current number.
+                        Otherwise, I move to the next number and repeat the same process until I find the smallest valid number.
 
 Date         : 06-08-2026
 Author       : Om Singh

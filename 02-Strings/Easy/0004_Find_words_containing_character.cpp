69c97e908dc07a30c0a5e8bce915609c3d61602b
Om Singh <omsinghleet@gmail.com>
2026-08-08 16:54:43 +0530

    Adding and solving brute

--- 02-Strings/Easy/0004_Find_words_containing_character.cpp
+++ 02-Strings/Easy/0004_Find_words_containing_character.cpp
@@ -16,21 +16,21 @@ Date         : 08-08-2026
 Author       : Om Singh
 ===============================================================================
 */
-
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve()
-{
-
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+        for (int i=0; i<words.size(); i++)
+        {
+            for (char ch : words[i])
+            {
+                if (ch == x) 
+                {
+                    ans.push_back(i);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};

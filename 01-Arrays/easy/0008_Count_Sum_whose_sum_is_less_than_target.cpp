6470b8c2cfe624469f50dce3ec3511e81047f59f
Om Singh <omsinghleet@gmail.com>
2026-08-09 22:55:57 +0530

    Adding todays problem

---
+++ 01-Arrays/easy/0008_Count_Sum_whose_sum_is_less_than_target.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Count Sum whose sum is less than target
+Platform     : LeetCode
+Pattern      : Arrays
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 09-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

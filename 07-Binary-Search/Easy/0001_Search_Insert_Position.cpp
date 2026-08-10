ebd206c3b57e3dbfd7fc419eee9ad13b812e6bb2
Om Singh <omsinghleet@gmail.com>
2026-08-10 09:23:03 +0530

    Adding BS Revision Problem

---
+++ 07-Binary-Search/Easy/0001_Search_Insert_Position.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Search Insert Position
+Platform     : LeetCode
+Pattern      : Binary Search
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 10-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

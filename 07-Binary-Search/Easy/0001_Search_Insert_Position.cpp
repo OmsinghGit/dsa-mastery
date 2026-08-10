2f75190e9927ed4919bec600ae619f3c53a0fc28
Om Singh <omsinghleet@gmail.com>
2026-08-10 09:34:34 +0530

    Adding solution

--- 07-Binary-Search/Easy/0001_Search_Insert_Position.cpp
+++ 07-Binary-Search/Easy/0001_Search_Insert_Position.cpp
@@ -17,20 +17,25 @@ Author       : Om Singh
 ===============================================================================
 */
 
-#include <bits/stdc++.h>
-using namespace std;
 
-void solve()
-{
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s = 0, e = nums.size() - 1;
 
-}
+        while (s <= e)
+        {
+            int mid = s + (e - s) / 2;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+            if (nums[mid] == target)
+                return mid;
 
-    solve();
+            if (nums[mid] < target)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
 
-    return 0;
-}
+        return s;
+    }
+};

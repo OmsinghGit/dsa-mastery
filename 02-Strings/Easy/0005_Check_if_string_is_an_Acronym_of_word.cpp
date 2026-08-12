57f604fc001ecbcc5159e6e32580899d91840df4
Om Singh <omsinghleet@gmail.com>
2026-08-12 23:50:54 +0530

    Adding string

---
+++ 02-Strings/Easy/0005_Check_if_string_is_an_Acronym_of_word.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Check if string is an Acronym of word
+Platform     : LeetCode
+Pattern      : Strings
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 12-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

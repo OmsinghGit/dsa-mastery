c290dc0194fe8d8bead6cf18daf15afd1b4d42d3
Om Singh <omsinghleet@gmail.com>
2026-08-13 07:06:47 +0530

    Adding solution

--- 02-Strings/Easy/0005_Check_if_string_is_an_Acronym_of_word.cpp
+++ 02-Strings/Easy/0005_Check_if_string_is_an_Acronym_of_word.cpp
@@ -21,3 +21,18 @@ Author       : Om Singh
 ===============================================================================
 */
 
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        int i=0;
+        if (words.size() != s.size())
+        return false;
+        while(i<words.size())
+        {
+            if(words[i][0] != s[i]) 
+            return false;
+            i++;
+        }
+        return true;
+    }
+};

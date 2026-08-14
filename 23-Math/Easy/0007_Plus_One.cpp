176e6fa26df228621fbcf37ea773905db6bf9ed1
Om Singh <omsinghleet@gmail.com>
2026-08-14 07:09:53 +0530

    Adding solution

--- 23-Math/Easy/0007_Plus_One.cpp
+++ 23-Math/Easy/0007_Plus_One.cpp
@@ -17,20 +17,20 @@ Author       : Om Singh
 ===============================================================================
 */
 
-#include <bits/stdc++.h>
-using namespace std;
 
-void solve()
-{
-
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i=digits.size()-1 ; i>=0; i--)
+        {
+            if(digits[i] < 9) 
+            {
+                digits[i]++;
+                return digits;
+            }
+            else digits[i]=0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};

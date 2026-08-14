03dbd28247b4af6628fb52d02991f35b3d6c2197
Om Singh <omsinghleet@gmail.com>
2026-08-14 14:59:02 +0530

    Duplicattion

---
+++ 01-Arrays/easy/0010_Return_Duplicate_Elements.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Return Duplicate Elements
+Platform     : Practice
+Pattern      : Arrays
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 14-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

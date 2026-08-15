9d625c4f795db5f4e05db54be7e8e1485951f090
Om Singh <omsinghleet@gmail.com>
2026-08-15 22:53:54 +0530

    Adding solution of today's question

--- 01-Arrays/medium/0004_Longest_Subsequence_with_Non-Zero_Bitwise_XOR.cpp
+++ 01-Arrays/medium/0004_Longest_Subsequence_with_Non-Zero_Bitwise_XOR.cpp
@@ -17,3 +17,19 @@ Author       : Om Singh
 ===============================================================================
 */
 
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int xorValue=0;
+        bool allZero = true;
+        for (int i=0;i<nums.size();i++)
+        {
+            xorValue ^= nums[i];
+            if (nums[i] != 0) allZero = false;
+        }
+        if (xorValue != 0) return nums.size();
+        if (allZero) return 0;
+
+        return nums.size()-1;
+    }
+};

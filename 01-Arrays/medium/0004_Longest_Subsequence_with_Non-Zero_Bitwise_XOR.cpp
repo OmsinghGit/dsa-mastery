797d6323e8b6c9caa8dd4d1b1d6620ad864b593d
Om Singh <omsinghleet@gmail.com>
2026-08-15 17:32:08 +0530

    Adding String and Streak Questions

---
+++ 01-Arrays/medium/0004_Longest_Subsequence_with_Non-Zero_Bitwise_XOR.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Longest Subsequence with Non-Zero Bitwise XOR
+Platform     : LeetCode
+Pattern      : Arrays
+Difficulty   : Medium
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 15-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

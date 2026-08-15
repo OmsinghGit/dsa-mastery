7e7801bdc87c5b3f0f5090da84c0ca9083abb2e6
Om Singh <omsinghleet@gmail.com>
2026-08-15 23:00:27 +0530

    Adding appraoch

--- 01-Arrays/medium/0004_Longest_Subsequence_with_Non-Zero_Bitwise_XOR.cpp
+++ 01-Arrays/medium/0004_Longest_Subsequence_with_Non-Zero_Bitwise_XOR.cpp
@@ -5,7 +5,11 @@ Platform     : LeetCode
 Pattern      : Arrays
 Difficulty   : Medium
 
-Approach     :
+Approach     : 1. Calculate XOR of whole array
+               2. if XOR != 0 return n
+               3. agr XOR = 0
+                    > if all elements are 0 - return 0
+                    > else return n-1
 
 Time Complexity  :
 Space Complexity :

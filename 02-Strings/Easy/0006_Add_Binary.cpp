bc778329ed197e980a7e254eb78499f14bbf4b44
Om Singh <omsinghleet@gmail.com>
2026-08-16 11:01:42 +0530

    Adding binary questions

---
+++ 02-Strings/Easy/0006_Add_Binary.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Add Binary
+Platform     : LeetCode
+Pattern      : Strings
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 16-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

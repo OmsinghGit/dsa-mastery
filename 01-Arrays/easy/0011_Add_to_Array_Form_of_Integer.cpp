823a3fd6f1630e48877524cd312236b42a77a2e2
Om Singh <omsinghleet@gmail.com>
2026-08-17 08:17:54 +0530

    feat: solved array integer question

--- 01-Arrays/easy/0011_Add_to_Array_Form_of_Integer.cpp
+++ 01-Arrays/easy/0011_Add_to_Array_Form_of_Integer.cpp
@@ -17,3 +17,61 @@ Author       : Om Singh
 ===============================================================================
 */
 
+// class Solution {
+// public:
+//     vector<int> addToArrayForm(vector<int>& num, int k) {
+//        int carry = 0;
+//        for (int i=num.size()-1 ; i>=0 ; i--)
+//        {
+//         int sum=num[i] + (k%10) + carry;
+//         num[i] = sum%10;
+//         carry = sum/10;
+//         k/=10;
+//         if (k==0 && carry ==0) break;
+//        }
+//        while (k>0)
+//        {
+//         int sum = (k%10) + carry ;
+//         num.insert(num.begin(), sum%10);
+//         carry = sum/10;
+//         k/=10;
+//        }
+//        if (carry) num.insert(num.begin(), carry);
+
+//        return num;
+//     }
+// };
+
+
+
+
+
+// Easier and Optimal too. 
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+
+        for (int i = num.size() - 1; i >= 0 && k > 0; i--)
+        {
+            int sum = num[i] + k;
+
+            num[i] = sum % 10;
+            k = sum / 10;
+        }
+
+        while (k > 0)
+        {
+            num.insert(num.begin(), k % 10);
+            k /= 10;
+        }
+
+        return num;
+    }
+};
+
+
+
+
+
+

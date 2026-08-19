f5eed910f05caccb312efc638e134271fde84c03
Om Singh <omsinghleet@gmail.com>
2026-08-19 22:10:29 +0530

    Docs : Adding new question

---
+++ 01-Arrays/easy/0012_Finding_Piviot_Index.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Finding Piviot Index
+Platform     : LeetCode
+Pattern      : Arrays
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 19-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

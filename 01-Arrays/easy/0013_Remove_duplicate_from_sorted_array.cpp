f93ac003ee53a5defe2af4669182a179f45e9106
Om Singh <omsinghleet@gmail.com>
2026-08-21 14:49:52 +0530

    Adding : docs new

---
+++ 01-Arrays/easy/0013_Remove_duplicate_from_sorted_array.cpp
@@ -0,0 +1,36 @@
+/*
+===============================================================================
+Problem      : Remove duplicate from sorted array
+Platform     : LeetCode
+Pattern      : Arrays
+Difficulty   : Easy
+
+Approach     :
+
+Time Complexity  :
+Space Complexity :
+
+Interview Explanation : 
+
+Date         : 21-08-2026
+Author       : Om Singh
+===============================================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}

ccf5b37bb8407030e70bef954ed2e9c29a095af4
Om Singh <omsinghleet@gmail.com>
2026-08-27 17:37:27 +0530

    docs: adding files and data 704

--- 07-Binary-Search/Easy/0002_Binary_Search.cpp
+++ 07-Binary-Search/Easy/0002_Binary_Search.cpp
@@ -5,12 +5,12 @@ Platform     : LeetCode
 Pattern      : Binary Search
 Difficulty   : Easy
 
-Approach     :
+Approach     : Use Binary Search. Check the middle element and eliminate half of the array each time.
 
-Time Complexity  :
-Space Complexity :
+Time Complexity  : O(log n)
+Space Complexity : O(1)
 
-Interview Explanation : 
+Interview Explanation : Since the array is sorted, I compare the target with the middle element and search only the possible half until the target is found.
 
 Date         : 27-08-2026
 Author       : Om Singh

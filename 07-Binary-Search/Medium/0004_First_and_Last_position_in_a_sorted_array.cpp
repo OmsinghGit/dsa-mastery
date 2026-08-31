03ce6c7e0c8689b9858e8ee5fb2e38a2a353cbd1
Om Singh <omsinghleet@gmail.com>
2026-08-31 15:01:21 +0530

    Docs : adding boiler codes

--- 07-Binary-Search/Medium/0004_First_and_Last_position_in_a_sorted_array.cpp
+++ 07-Binary-Search/Medium/0004_First_and_Last_position_in_a_sorted_array.cpp
@@ -17,20 +17,15 @@ Author       : Om Singh
 ===============================================================================
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve()
-{
-
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    solve();
-
-    return 0;
-}
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = 0, end = nums.size()-1;
+        while (left <= right)
+        {
+            int mid = start + (end - start)/2;
+            
+        }
+        return [-1.-1];
+    }
+};

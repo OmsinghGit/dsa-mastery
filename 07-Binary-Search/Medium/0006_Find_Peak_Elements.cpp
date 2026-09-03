900a9c100a53c8009716b661e5ba3b88befa835a
Om Singh <omsinghleet@gmail.com>
2026-09-03 19:40:10 +0530

    docs: approach,complexities

--- 07-Binary-Search/Medium/0006_Find_Peak_Elements.cpp
+++ 07-Binary-Search/Medium/0006_Find_Peak_Elements.cpp
@@ -5,15 +5,41 @@ Platform     : LeetCode
 Pattern      : Binary Search
 Difficulty   : Medium
 
-Approach     :
+Approach     : Use Binary Search on the slope of the array. Compare nums[mid] with nums[mid + 1]:
 
-Time Complexity  :
-Space Complexity :
+                > If nums[mid] < nums[mid + 1], the peak is on the right.
+                > Otherwise, the peak is on the left or at mid.
+                > When left == right, that index is a peak.
 
-Interview Explanation : 
+Time Complexity  : O(log n)
+Space Complexity : O(1)
+
+Interview Explanation : I use Binary Search by comparing the middle element with the next element. If the array is increasing, 
+                        I move right; otherwise, I move left while keeping mid as a possible peak. When both pointers meet, that index is the peak.
 
 Date         : 02-09-2026
 Author       : Om Singh
 ===============================================================================
 */
 
+
+int left = 0;
+int right = nums.size() - 1;
+
+while (left < right)
+{
+    int mid = left + (right - left) / 2;
+
+    if (nums[mid] < nums[mid + 1])
+    {
+        // increasing → peak RIGHT me
+        left = mid + 1;
+    }
+    else
+    {
+        // decreasing → peak LEFT ya MID me
+        right = mid;
+    }
+}
+
+return left;
